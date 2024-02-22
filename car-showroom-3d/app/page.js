7eb6f1f5775aa86a187667edbca684c0558c1838
MidnightDemise <midnightdemise123@gmail.com>
2024-02-22 13:58:27 +0500

    landing page done almost
    now doing the featured cars

--- car-showroom-3d/app/page.js
+++ car-showroom-3d/app/page.js
@@ -3,59 +3,42 @@
 import MainPageOriginal from "@/components/MainPageOriginal";
 import MainPage from "@/components/MainPageTest";
 import SmokeTest from "@/components/SmokeTest";
-import { Float, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { NodeToyMaterial, NodeToyTick } from "@nodetoy/react-nodetoy";
+import { Float, OrbitControls, PerspectiveCamera, TransformControls, useFBX } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useEffect, useState } from "react";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from 'three'
+import vertexShader from "@/components/smokeVertex";
+import fragmentShader from "@/components/smokeFragment";
+
+
+
+
+
 
 export default function Home() {
-  const [particles , setParticles] = useState([]);
+  const crateGroup = useRef();
+  const scene = useFBX("Arm Stretching.fbx").animations;
 
-
-  useEffect(() => {
-    // Create a particle system
-    const particleCount = 1000;
-    const particleGeometry = new THREE.BufferGeometry();
-    const particleMaterial = new THREE.PointsMaterial({
-      color: 0x888888,
-      size: 0.1,
-    });
-    const positions = [];
-    for (let i = 0; i < particleCount; i++) {
-      const x = Math.random() * 10 - 5;
-      const y = Math.random() * 2 - 1;
-      const z = Math.random() * 10 - 5;
-      positions.push(x, y, z);
-    }
-    particleGeometry.setAttribute(
-      "position",
-      new THREE.Float32BufferAttribute(positions, 3)
-    );
-    const particleSystem = new THREE.Points(
-      particleGeometry,
-      particleMaterial
-    );
-    setParticles(particleSystem);
-  }, []);
-
+  console.log(scene);
 
 
   return (
    <>
       <Canvas flat linear style={{width: "100vw", height: "100vh"}}>
-        <color args={[1,1,1]}  attach={"background"}/>
-        
-        {/*         
-        <Float floatingRange={0.1} rotationIntensity={0.2} floatIntensity={0.3}>
-          <PerspectiveCamera makeDefault position={[4,0,4]} rotation={[0,Math.PI - Math.PI/4 , 0]} fov={45} far={1000} near={1}/>
-        </Float> */}
-        <OrbitControls/>
-        <ambientLight/>
+        <color args={[0,0,0]}  attach={"background"}/>
+        <ambientLight />
         <directionalLight/>
-        <mesh position={[0,-2,0]} rotation={[-Math.PI / 2 , 0, 0]}>
-          <planeGeometry args={[30,30]}/>
-          <meshBasicMaterial color={"black"}/>
-        </mesh>
+        
+        
+           
+         {/* <Float floatingRange={0.1} rotationIntensity={0.2} floatIntensity={0.3}>
+          <PerspectiveCamera makeDefault position={[4,0,4]} rotation={[0,Math.PI - Math.PI/4 , 0]} fov={45} far={1000} near={1}/>
+        </Float>   */}
+    
+      {/* <MainPageOriginal/> */}
+       
         
 
 
@@ -64,3 +47,20 @@ export default function Home() {
    </>
   )
 }
+   {/* <OrbitControls makeDefault/>   */}
+       {/* <ambientLight/>
+        <directionalLight/>*/}
+        {/* <NodeToyTick/> */}
+        
+        {/* 
+        <TransformControls>
+        <mesh>
+          <boxGeometry attach="geometry" args={[1, 1, 1]} />
+          <NodeToyMaterial  parameters={{
+                lights: false, // Disable lights affecting the material
+                envMap: false, // Disable environment map affecting the material
+              }} url={"https://draft.nodetoy.co/OUsvR8blhs7VwoIX"} />
+        </mesh> 
+        </TransformControls>
+        */
+      }
