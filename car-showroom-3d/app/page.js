a3969bc3033cc533f6de968f675bcb40bf5bb312
MidnightDemise <midnightdemise123@gmail.com>
2024-02-24 23:33:11 +0500

    idk
    we

--- car-showroom-3d/app/page.js
+++ car-showroom-3d/app/page.js
@@ -1,47 +1,62 @@
 'use client'
 
 import MainPageOriginal from "@/components/MainPageOriginal";
-import { Environment, Float, OrbitControls, PerspectiveCamera, TransformControls, useFBX } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Box, Environment, Float, OrbitControls, PerspectiveCamera, Sphere, TransformControls, useFBX } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from 'three'
 import FeaturedCarsPage from "@/components/FeaturedCarsPage";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { Physics } from "@react-three/rapier";
-
-
-
-
-
+import Morphing from "@/components/test/Morphing";
 
 export default function Home() {
 
-
+  
 
 
   return (
    <>
       <Canvas flat linear style={{width: "100vw", height: "100vh"}}>
-        <color args={[ 0 , 0 ,0]}  attach={"background"}/>
+        <color args={[ 0 , 0, 0]}  attach={"background"}/>
         <ambientLight intensity={15}/>
-        <PerspectiveCamera makeDefault position={[0, 11, 20]}/>
-        {/* <OrbitControls autoRotate autoRotateSpeed={3} camera={undefined} enablePan={false} enableZoom={false} /> */}
-        <OrbitControls/>
-       
-     
+        <directionalLight/>
+        
 
+      <PerspectiveCamera makeDefault position={[0, 11, 20]}/> 
+         <OrbitControls autoRotate autoRotateSpeed={3} camera={undefined} enablePan={false} enableZoom={false} /> 
+        
+        <Morphing/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       <Suspense>
+         <Physics debug>
+          
+        <FeaturedCarsPage/> 
+
+        </Physics> 
+
+
+    
 {/*            
           <Float floatingRange={0.1} rotationIntensity={0.2} floatIntensity={0.3}>
           <PerspectiveCamera makeDefault position={[4,0,4]} rotation={[0,Math.PI - Math.PI/4 , 0]} fov={45} far={1000} near={1}/>
         </Float>    */}
     
       {/* <MainPageOriginal/>  */}
-       
-       <Suspense>
-        <Physics debug>
-          <FeaturedCarsPage/> 
-
-        </Physics>
-
        </Suspense>
 
       </Canvas>
---
+++ car-showroom-3d/components/test/Morphing.js
@@ -0,0 +1,105 @@
+import React, { useRef, useEffect } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+const Morphing = () => {
+  const groupRef = useRef();
+  const meshRef = useRef();
+  const geometryRef = useRef();
+
+  const { vertices } = useLoader(OBJLoader, "models/face_1.obj");
+  const { vertices: vertices2 } = useLoader(OBJLoader, "models/face_2.obj");
+  const { vertices: vertices3 } = useLoader(OBJLoader, "models/face_3.obj");
+  const { vertices: vertices4 } = useLoader(OBJLoader, "models/skull_4.obj");
+
+  useEffect(() => {
+    const numParticles = 25000;
+    const particlesPosition = new Float32Array(numParticles * 3);
+
+    for (let i = 0; i < numParticles; i++) {
+      const vertexIndex = i % vertices.length;
+      const position = vertices[vertexIndex];
+      particlesPosition.set([position.x, position.y, position.z], i * 3);
+    }
+
+    geometryRef.current = new THREE.BufferGeometry();
+    geometryRef.current.setAttribute("position", new THREE.BufferAttribute(particlesPosition, 3));
+  }, [vertices]);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.008;
+    }
+  });
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.timeline({
+      defaults: {},
+      scrollTrigger: {
+        trigger: "body",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.7,
+      },
+    }).to(meshRef.current.rotation, {
+      y: Math.PI * 2,
+    });
+
+    const uniforms = {
+      u_sec1: { value: 0.0 },
+      u_sec2: { value: 0.0 },
+      u_sec3: { value: 0.0 },
+      u_sec4: { value: 0.0 },
+    };
+
+    const material = new THREE.RawShaderMaterial({
+      uniforms,
+      transparent: true,
+      blending: THREE.AdditiveBlending,
+    });
+
+    meshRef.current = new THREE.Points(geometryRef.current, material);
+    groupRef.current.add(meshRef.current);
+
+    gsap.to(meshRef.current.material.uniforms.u_sec1, {
+      value: 1.0,
+      scrollTrigger: {
+        trigger: ".s-1",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: 0.7,
+      },
+    });
+
+    gsap.to(meshRef.current.material.uniforms.u_sec2, {
+      value: 1.0,
+      scrollTrigger: {
+        trigger: ".s-2",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: 0.7,
+      },
+    });
+
+    gsap.to(meshRef.current.material.uniforms.u_sec3, {
+      value: 1.0,
+      scrollTrigger: {
+        trigger: ".s-3",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: 0.7,
+      },
+    });
+  }, []);
+
+  return (
+    <group ref={groupRef} />
+  );
+};
+
+export default Morphing;
