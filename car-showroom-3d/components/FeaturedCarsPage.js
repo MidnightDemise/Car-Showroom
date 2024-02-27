ffbdd9d8a49f12d00e40fc0b3e071dc3202a300a
MidnightDemise <midnightdemise123@gmail.com>
2024-02-27 23:09:17 +0500

    added wheel rotations
    new cars and wheel rotations for some cars

--- car-showroom-3d/components/FeaturedCarsPage.js
+++ car-showroom-3d/components/FeaturedCarsPage.js
@@ -13,6 +13,8 @@ import { WorkingDriver } from './Workingdriver';
 import { WorkingDriverCopy } from '@/public/Workingdriver2';
 import { DriverTexting } from './Drivertext';
 import Car from './FeaturedCarsPageComponents/LamboCover';
+import { TestGLTF } from './FeaturedCarsPageComponents/Scene';
+
 
 
 
@@ -46,11 +48,11 @@ const FeaturedCarsPage = () => {
   
 
   const cars = [
-     { carModel: "Challenger", rotation: [0, Math.PI, 0], position: [0, 1, -8], scale: 0.03, name: "Challenger", isSelected: selectedCar === "Challenger" },
-     { carModel: "Lambo", rotation: [0, 0, 0], position: [0, 1, -22], scale: 5, name: "Lambo", isSelected: selectedCar === "Lambo" },
-    { carModel: "RocketLeague", rotation: [0, -Math.PI / 2, 0], position: [0, 1, -22], scale: 0.1, name: "RocketLeague", isSelected: selectedCar === "RocketLeague" },
-  { carModel: "Bugatti", rotation: [0, Math.PI, 0], position: [0, 1, -25], scale: 3.5, name: "Bugatti", isSelected: selectedCar === "Bugatti" }
-//  { carModel: "test", rotation: [0, Math.PI, 0], position: [0, 1, -25], scale: 0.1, name: "test", isSelected: selectedCar === "test" }
+    //   { carModel: "Challenger", rotation: [0, Math.PI, 0], position: [0, 1, -8], scale: 0.03, name: "Challenger", isSelected: selectedCar === "Challenger" },
+    //  { carModel: "Lambo", rotation: [0, 0, 0], position: [0, 1, -22], scale: 5, name: "Lambo", isSelected: selectedCar === "Lambo" },
+   // { carModel: "ETron", rotation: [0, 0 , 0], position: [0, 1, -22], scale: 0.4, name: "ETron", isSelected: selectedCar === "ETron" },
+  //  { carModel: "Bugatti", rotation: [0, Math.PI, 0], position: [0, 1, -25], scale: 3.5, name: "Bugatti", isSelected: selectedCar === "Bugatti" },
+  // { carModel: "test", rotation: [0, 0, 0], position: [0, 3.2, -25], scale: 3, name: "test", isSelected: selectedCar === "test" }
   ];
   return (
     <>
@@ -73,23 +75,27 @@ const FeaturedCarsPage = () => {
 
           
             <Showroom />
-              <Driver scale={3.5} rotation={[0 , Math.PI / 2 , 0]} position={[-6,1,0]}/>
+              
               <WorkingDriver scale={3.5} rotation={[0 , Math.PI / 2 , 0]} position={[12,0,5]}/>
               <WorkingDriverCopy scale={3.5} rotation={[0 , -Math.PI / 2 , 0]} position={[-12,0,5]}/>
               <DriverTexting scale={3.5} rotation={[0 , -Math.PI / 2 , 0]} position={[6,1,0]}/>
+              
+              
+              {/* <BugattiGLTF scale={3.5} position={[0,1,0]} rotation={[Math.PI/2,Math.PI ,-Math.PI]}/> */}
+              {/* <LamboGLTF position={[0,1,0]} scale={5}/> */}
               {/* <Lambo  position= {[0,1,0]} scale={5} />  */}
-              {cars.map((car, index) => (
-                <Car
-                  key={index}
-                  carModel={car.carModel}
-                  rotation={car.rotation}
-                  position={car.position}
-                  scale={car.scale}
-                  name={car.name}
-                  onClick={() => handleCarSelection(car.name)}
-                  isSelected={car.isSelected}
-                />
-              ))}
+               {cars.map((car, index) => (
+                 <Car
+                   key={index}
+                carModel={car.carModel}
+                   rotation={car.rotation}
+                   position={car.position}
+                   scale={car.scale}
+                   name={car.name}
+                   onClick={() => handleCarSelection(car.name)}
+                   isSelected={car.isSelected}
+                 />
+               ))}  
               {/* <OpenText scale={5} position={[0,2,0]}/> */}
              
 
--- car-showroom-3d/components/FeaturedCarsPageComponents/LamboCover.js
+++ car-showroom-3d/components/FeaturedCarsPageComponents/LamboCover.js
@@ -17,6 +17,8 @@ import dissolveFragment from "../CarShader/CarCoverDissolveFragment";
 
 export default function Car(props) {
     const {carModel , isSelected} = props;
+    const [carWheels, setCarWheels] = useState([]);
+
     const { scene, nodes, materials } = useGLTF(`FeaturedCars/${carModel}/scene.gltf`);
     const car = useRef();
     const [isClick, setIsClicked] = useState(false);
@@ -43,7 +45,7 @@ export default function Car(props) {
             transparent: true
         });
 
-
+        
         Object.values(nodes).forEach((node) => {
 
             if(originalMaterials[node.uuid] != null)
@@ -61,6 +63,9 @@ export default function Car(props) {
                     }
                 }
             }
+
+
+           
            
         });
     
@@ -68,10 +73,23 @@ export default function Car(props) {
         if (isSelected && car.current.position.z < 0)
         {
             car.current.position.z += 0.1;
+
+            carWheels.forEach((wheel) => {
+            
+                wheel.rotation.x -= 0.1; // Rotate around the x-axis
+            });
         }
         else if(!isSelected && car.current.position.z > -22)
         {
             car.current.position.z -= 0.1;
+           
+        }
+        if(!isSelected)
+        {
+            carWheels.forEach((wheel) => {
+            
+                wheel.rotation.x += 0.1; // Rotate around the x-axis
+            });
         }
 
     });
@@ -81,14 +99,20 @@ export default function Car(props) {
         // Store original materials
         Object.values(nodes).forEach((node) => {
             originalMaterials[node.uuid] = node.material;
+
+            // if (node.name === "anteriore_dest_20" || node.name === "anteriore_sins_30" || node.name === "ppst_sinis_115" || node.name === "post_dest_105")  {
+            //    
+            // }
+            if(node.name === "geo_rim_tileable3_Ghostrim_0" || node.name === "geo_rim_tileable2_Ghostrim_0" || node.name === "geo_rim_tileable1_Ghostrim_0" || node.name === "geo_rim_tileable_Ghostrim_0")
+                setCarWheels(prevWheels => [...prevWheels, node]); // Add node to the carWheels array
+
         });
 
         Object.values(materials).forEach((material) => {
-            console.log(material);
-
             material.envMapIntensity = 10;
 
         })
+
     }, []);
 
     // Revert shader to original material
